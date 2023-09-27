13f1bfdc66f728763619f94702dd447991c882bc
Sebastian G. Q. <sgq995@gmail.com>
2023-09-26 20:04:47 -0500

    feat: trpc client

---
+++ packages/trpc/client/index.ts
@@ -0,0 +1,6 @@
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "../server";
+
+export const trpc = createTRPCProxyClient<AppRouter>({
+  links: [httpBatchLink({ url: "http://localhost:3000/trpc" })],
+});
